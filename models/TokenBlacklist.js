8b37189dba426a5ae196a54b33bb460c18ea0716
amankaushik2810 <amankaushik2810@gmail.com>
2026-08-16 18:46:00 +0530

    Remove old root files and organize frontend and backend Structure

--- models/TokenBlacklist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const TokenBlacklistSchema = new mongoose.Schema({
-  token: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: "1h" }
-});
-
-module.exports = mongoose.model("TokenBlacklist", TokenBlacklistSchema);
